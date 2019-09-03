f3fbdbb7989d221368bd92854ce01cbdd0ca1750
Kumar Abhirup <kumar@iqubex.com>
2019-09-03 19:33:11 +0530

    📦 NEW: v0.1.0: Add tableName parameter

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 [![Type](https://img.shields.io/badge/type-API-yellow.svg?style=flat-square)](https://www.npmjs.com/package/koji-leaderboard-api)
 [![stage](https://img.shields.io/badge/stage-BetaTesting%20%F0%9F%94%A5-000000.svg?style=flat-square)](https://github.com/KumarAbhirup/koji-leaderboard-api)
 [![npm](https://img.shields.io/badge/npm-koji--leaderboard--api-CB3837.svg?style=flat-square)](https://www.npmjs.com/package/koji-leaderboard-api)
-[![Version](https://img.shields.io/badge/version-v0.0.9-green.svg?style=flat-square)](https://www.npmjs.com/package/koji-leaderboard-api)
+[![Version](https://img.shields.io/badge/version-v0.1.0-green.svg?style=flat-square)](https://www.npmjs.com/package/koji-leaderboard-api)
 [![Prefers](https://img.shields.io/badge/prefers-NPM%20Installation-blue.svg?style=flat-square)](https://www.npmjs.com/package/koji-leaderboard-api)
 [![Twitter](https://img.shields.io/twitter/follow/kumar_abhirup.svg?style=social&label=@kumar_abhirup)](https://twitter.com/kumar_abhirup)
 <!-- [![GitHub stars](https://img.shields.io/github/stars/KumarAbhirup/koji-leaderboard-api.svg?style=social&label=Stars)](https://github.com/KumarAbhirup/koji-leaderboard-api) -->
@@ -18,8 +18,6 @@
 
 # 📦 `koji-leaderboard-api`
 ### **Koji Database based Leaderboard API setup, at your doorsteps.**
-
----
 
 When you create games or any competitive webapp on Koji, you need to have Leaderboards. Setting them up in the backend could sometimes be a horrific task. So, you have this simple **plugNplay Express API** that runs smoothly with the most minimal setup!
 
@@ -34,6 +32,15 @@ To install the API on your Node.js backend, run the following.
 ```bash
 $ npm i -S koji-leaderboard-api
 ```
+
+## Summary
+
+```js
+kojiLeaderboardApi(app, 'leaderboard')
+```
+
+- 1st parameter: `app` 👉 (type: `Express App Instance`, Required)
+- 2nd parameter: `tableName` 👉 (type: `String`, Optional, Default: `leaderboard`) - The table name you want your data to be stored under. For eg. if your `tableName` is `leaderboard`, then your API endpoints will be `GET /leaderboard` and `POST /leaderboard`.
 
 <br />
 
@@ -67,6 +74,9 @@ app.use((req, res, next) => {
  * @name kojiLeaderboardApi
  * @description Doing `kojiLeaderboardApi(app)` activates the `/leaderboard` GET and POST API endpoints
  *              that your frontend can use to Display and Update Leaderboard
+ * 
+ * @param {Express App Instance} app - (required)
+ * @param {String} tableName - (optional) Default: `leaderboard`
  */
 kojiLeaderboardApi(app)
 
@@ -85,6 +95,10 @@ In the above example, we make use of an express server. What you need to do, is 
 |---|------------|----------------|--------------------------------------------------------------------------------------------------------|
 | 1 | GET        | `/leaderboard` |                                                    -                                                   |
 | 2 | POST       | `/leaderboard` | name: String 👈 _required_ <br /> score: Number 👈 _required_ <br /> privateAttributes: Object 👈 _optional_ |
+
+<br />
+
+> **Important note:** The above mentioned API endpoints only work if you haven't filled in the second parameter. If you have, the second parameter will be your GET and POST endpoint.
 
 <br />
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Database from '@withkoji/database'
 import uuid from 'uuid'
+import express from 'express'
 
 /* eslint-disable prettier/prettier */
 interface Idata {
@@ -11,73 +12,79 @@ interface Idata {
 }
 /* eslint-enable prettier/prettier */
 
-export default (app: any): void => {
+export default (app: express.Application, tableName = 'leaderboard'): void => {
   // To get the Leaderboard Scores
-  app.get('/leaderboard', async (req: any, res: any) => {
-    const database = new Database()
-    const rawScores = await database.get('leaderboard')
-
-    /**
-     * @description We don't want to return private attributes to consumers of this
-     *              endpoint, so strip them out, sort the records so the top scores
-     *              appear first, and then only return the top 100 scores
-     */
-    const scores = rawScores
-      .map(
-        (data: Idata): Idata => ({
-          name: data.name,
-          score: data.score,
-          dateCreated: data.dateCreated,
-        })
-      )
-      .sort((a: Idata, b: Idata) => b.score - a.score)
-      .slice(0, 100)
-
-    res.status(200).json({
-      success: true,
-      scores,
-    })
-  })
-
-  // To save the Leaderboard Scores
-  app.post('/leaderboard', async (req: any, res: any) => {
-    const recordId = uuid.v4()
-
-    const recordBody: Idata = {
-      name: req.body.name,
-      score: req.body.score,
-      privateAttributes: req.body.privateAttributes,
-      dateCreated: Math.round(Date.now() / 1000),
-    }
-
-    if (
-      typeof recordBody.name === 'string' &&
-      recordBody.name.length > 3 &&
-      typeof recordBody.score === 'number' &&
-      recordBody.score > 1 &&
-      (typeof recordBody.privateAttributes === 'object' ||
-        recordBody.privateAttributes === null)
-    ) {
+  app.get(
+    `/${tableName}`,
+    async (req: express.Request, res: express.Response) => {
       const database = new Database()
-      await database.set('leaderboard', recordId, recordBody)
+      const rawScores = await database.get(tableName)
+
+      /**
+       * @description We don't want to return private attributes to consumers of this
+       *              endpoint, so strip them out, sort the records so the top scores
+       *              appear first, and then only return the top 100 scores
+       */
+      const scores = rawScores
+        .map(
+          (data: Idata): Idata => ({
+            name: data.name,
+            score: data.score,
+            dateCreated: data.dateCreated,
+          })
+        )
+        .sort((a: Idata, b: Idata) => b.score - a.score)
+        .slice(0, 100)
 
       res.status(200).json({
         success: true,
-        data: {
-          ...recordBody,
-        },
-      })
-    } else {
-      res.status(400).json({
-        success: false,
-        error: {
-          message: 'Invalid, Incomplete or Inaccurate data provided',
-          developerNotice:
-            'Please read the documentation carefully and provide the correct parameters.',
-          documentationURL:
-            'https://github.com/KumarAbhirup/koji-leaderboard-api',
-        },
+        scores,
       })
     }
-  })
+  )
+
+  // To save the Leaderboard Scores
+  app.post(
+    `/${tableName}`,
+    async (req: express.Request, res: express.Response) => {
+      const recordId = uuid.v4()
+
+      const recordBody: Idata = {
+        name: req.body.name,
+        score: req.body.score,
+        privateAttributes: req.body.privateAttributes,
+        dateCreated: Math.round(Date.now() / 1000),
+      }
+
+      if (
+        typeof recordBody.name === 'string' &&
+        recordBody.name.length > 3 &&
+        typeof recordBody.score === 'number' &&
+        recordBody.score > 1 &&
+        (typeof recordBody.privateAttributes === 'object' ||
+          recordBody.privateAttributes === null)
+      ) {
+        const database = new Database()
+        await database.set(tableName, recordId, recordBody)
+
+        res.status(200).json({
+          success: true,
+          data: {
+            ...recordBody,
+          },
+        })
+      } else {
+        res.status(400).json({
+          success: false,
+          error: {
+            message: 'Invalid, Incomplete or Inaccurate data provided',
+            developerNotice:
+              'Please read the documentation carefully and provide the correct parameters.',
+            documentationURL:
+              'https://github.com/KumarAbhirup/koji-leaderboard-api',
+          },
+        })
+      }
+    }
+  )
 }
